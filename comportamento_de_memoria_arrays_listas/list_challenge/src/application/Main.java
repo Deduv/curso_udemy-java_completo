78321cda75289d6eab2218765e45b3b0c0be9db6
Deduv <edusant999@gmail.com>
2026-03-18 23:27:02 -0300

    feat: list challenge

---
+++ comportamento_de_memoria_arrays_listas/list_challenge/src/application/Main.java
@@ -0,0 +1,71 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List <Employee> list = new ArrayList<>();
+		
+		System.out.print("How many employees will be registred? ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println();
+			System.out.println("Employee #" + (i + 1) + ":");
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			while (hasId(list, id)) {
+				System.out.println("Id already taken! Try again: ");
+				id = sc.nextInt();
+			}
+			
+			sc.nextLine();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			
+			Employee emp = new Employee (id, name, salary);
+			
+			list.add(emp);
+		}
+		
+		System.out.println();
+		System.out.print("Enter the employee id that will have salary increase : ");
+		int id2 = sc.nextInt();
+		
+		Employee emp = list.stream().filter(x -> x.getId() == id2).findFirst().orElse(null);
+		
+		if (emp == null) {
+			System.out.println("This id does not exist!");
+		} 
+		else {
+			System.out.print("Enter the percentage : ");
+			double percent = sc.nextDouble();
+			emp.increaseSalary(percent);
+		}
+		
+		System.out.println();
+		System.out.println("List of employees: ");
+		for (Employee e : list) {
+			System.out.println(e.toString());
+		}
+		
+		
+		sc.close();
+	}
+	
+	public static boolean hasId(List<Employee> list, int id) {
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
+	}
+}
