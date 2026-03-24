53ddfaf870913c16d88feb7584361d5a131e8abb
Deduv <edusant999@gmail.com>
2026-03-24 14:52:39 -0300

    feat: employee payment system using inheritance and polymorphism

---
+++ herança_e_polimorfismo/ex01/src/application/Main.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		Locale.setDefault(Locale.US);
+		
+		String name = sc.nextLine();
+		int workedHours = sc.nextInt();
+		double valuePerHour = sc.nextDouble();
+		
+		sc.close();
+	}
+
+}
