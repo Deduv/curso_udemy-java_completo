fa370812c052a00e8a3f7878b92e7e9e4734a5bd
Deduv <edusant999@gmail.com>
2026-04-09 15:30:10 -0300

    Solution with interface and abstract class

--- interfaces/HerdarVsCumprir/src/application/Program.java
+++ interfaces/HerdarVsCumprir/src/application/Program.java
@@ -2,9 +2,9 @@ package application;
 
 import java.util.Locale;
 
+import model.entities.AbstractShape;
 import model.entities.Circle;
 import model.entities.Rectangle;
-import model.entities.Shape;
 import model.enums.Color;
 
 public class Program {
@@ -13,8 +13,8 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		
-		Shape s1 = new Circle(Color.Black, 2.0);
-		Shape s2 = new Rectangle(Color.White, 3.0, 4.0);
+		AbstractShape s1 = new Circle(Color.Black, 2.0);
+		AbstractShape s2 = new Rectangle(Color.White, 3.0, 4.0);
 		
 		System.out.println("Circle color: " + s1.getColor());
 		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
