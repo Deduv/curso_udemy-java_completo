3c966d6bd2c338ee89bd516d7b46cc90d867e330
Deduv <edusant999@gmail.com>
2026-04-17 22:39:35 -0300

    feat: implement flexible sorting using Comparator and Lambda expressions

---
+++ programacao_funcional_e_expressoes_lambda/flexible-product-sorting/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List <Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		
+		for(Product p : list) {
+			System.out.println(p.toString());
+		}
+		
+		sc.close();
+	}
+
+}
