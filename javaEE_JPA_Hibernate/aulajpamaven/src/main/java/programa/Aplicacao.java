827d6ce8d360dbcfba9e19730277b547406bf137
Deduv <edusant999@gmail.com>
2026-04-29 15:39:00 -0300

    feat: implement entity retrieval and removal using JPA EntityManager

--- javaEE_JPA_Hibernate/aulajpamaven/src/main/java/programa/Aplicacao.java
+++ javaEE_JPA_Hibernate/aulajpamaven/src/main/java/programa/Aplicacao.java
@@ -1,18 +1,30 @@
 package programa;
 
 import dominio.Pessoa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Aplicacao {
 
     static void main (String[] args) {
 
-        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        Pessoa p = em.find(Pessoa.class, 2);
+
+        em.getTransaction().begin();
+
+        em.remove(p);
+
+        em.getTransaction().commit();
+
+        System.out.println("Pronto !");
+        em.close();
+        emf.close();
 
     }
 }
